eae90a7f234b7ab0b1054c5ff47e020aa0343500
BUMBAIYA <amitchauhan263871@gmail.com>
2026-01-02 14:10:00 +0530

    chore: update styles

--- src/games/2048/components/GamePage.tsx
+++ src/games/2048/components/GamePage.tsx
@@ -9,7 +9,6 @@ export function GamePage() {
     gameManager = new GameManager(gameBoardContainer);
     gameManager.startNewGame();
 
-    gameManager.attachListeners();
     console.log(gameManager);
 
     onCleanup(() => {
@@ -23,16 +22,16 @@ export function GamePage() {
 
   return (
     <>
-      <header class="flex items-center justify-between">
+      <header class="flex items-center justify-between p-5">
         <h1 class="text-4xl font-bold">2048</h1>
         <button
-          class="bg-accent text-background focus:ring-accent cursor-pointer rounded-lg px-3 py-1.5 ring-2 transition-all focus:scale-105 focus:ring-2 focus:ring-offset-2"
+          class="bg-accent text-background focus:ring-accent cursor-pointer rounded-lg px-3 py-1.5 ring-2 transition-all hover:scale-110 focus:scale-110 focus:ring-2 focus:ring-offset-2"
           onClick={handleStartNewGame}
         >
-          <span>New Game</span>
+          <span class="text-xl font-medium">New Game</span>
         </button>
       </header>
-      <main class="flex flex-1 flex-col items-center justify-center p-3">
+      <main class="flex flex-1 flex-col items-center justify-center p-5">
         <div ref={gameBoardContainer} class="max-h-[450px] w-full flex-1" />
       </main>
     </>
